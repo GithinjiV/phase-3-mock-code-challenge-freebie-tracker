e4c973cd5670b6ca7ca7c090236437b421c58da1
vinnie <githinjigicheru146@gmail.com>
2024-04-25 14:24:16 +0300

    Create a freebie tracker using active record

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,18 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+    def freebies
+        self.freebies
+    end
+    def devs
+        self.devs
+    end
 
+    def give_freebies(dev:dev, item_name:item, value:value)
+        Freebie.create(item_name:item, value:value, dev:dev, company:self)
+    end
+
+    def self.oldest_company
+        self.order(:founding_year).first
+    end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,23 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def freebies
+        self.freebies
+    end
+
+    def companies
+        self.companies
+    end
+
+    def received_one?(item_name)
+        self.freebies.any? do |freebie|
+            freebie.item_name == item_name
+        end
+    end
+
+    def give_away(freebie, dev)
+        freebie.update(dev: dev) unless freebie.dev != self
+    end
 
 end
